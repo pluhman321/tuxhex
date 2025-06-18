04a33911f454c152b96e775b8ee249c2b9bdd075
pluhman321 <hairs.parade_0g@icloud.com>
2025-06-18 05:03:08 -0700

    Update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>🚀 TuxHex is live!</h1><p>This is your web dashboard.</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
